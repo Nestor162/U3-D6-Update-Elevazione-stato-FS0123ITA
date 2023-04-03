1d82f9664fc48f0f1483a7b64abe3601783055f3
Nestor162 <nestorcicardini@gmail.com>
2023-04-03 14:37:22 +0200

    Creato componente CommentArea

---
+++ src/components/CommentArea .jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+import CommentList from "./CommentList";
+
+class CommentArea extends Component {
+  render() {
+    return <CommentList></CommentList>;
+  }
+}
+export default CommentArea;
